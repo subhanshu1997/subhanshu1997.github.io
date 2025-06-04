0fff83159fd155fa922be1a9769a677866803b7c
Subhanshu Bigasia <subhanshu.bigasia@carelon.com>
2025-06-04 18:05:37 +0100

    Updates

---
+++ precache-manifest.9e1d3ee33a678d67239484d58507bc79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9132d977432dcc5a283e3dfdcd30f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97a780f64aea2b83e3e5",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "27184cc10ef662e5989a",
+    "url": "/static/css/main.762d7760.chunk.css"
+  },
+  {
+    "revision": "97a780f64aea2b83e3e5",
+    "url": "/static/js/2.83d01389.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.83d01389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27184cc10ef662e5989a",
+    "url": "/static/js/main.0b08422d.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
